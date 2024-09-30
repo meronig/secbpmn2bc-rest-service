c75a36652fb342b2a9121ace4928438c62522863
Giovanni Meroni <giom@dtu.dk>
2024-09-30 17:54:19 +0200

    Extended API for annotating BPMN model based on secbpmn results.

--- dk.dtu.compute.secbpmn2bc.rest/src/main/java/dk/dtu/compute/secbpmn2bc/bpmnimporter/rest/Application.java
+++ dk.dtu.compute.secbpmn2bc.rest/src/main/java/dk/dtu/compute/secbpmn2bc/bpmnimporter/rest/Application.java
@@ -5,10 +5,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
@@ -32,14 +42,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import dk.dtu.compute.secbpmn2bc.bpmnimporter.files.Bpmn2secbpmnbc;
 import it.polimi.isgroup.secbpmn2bc.model.infer.ConsoleMessage;
 import it.polimi.isgroup.secbpmn2bc.model.infer.InferBCProperties;
 import it.polimi.isgroup.secbpmn2bc.model.meta.SecBPMN2BCPackage;
 import it.polimi.isgroup.secbpmn2bc.model.util.SecBPMN2BCResourceFactoryImpl;
-
 
 @SpringBootApplication
 @CrossOrigin(maxAge = 3600)
@@ -50,7 +62,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@PostMapping(value="/convert", produces=MediaType.APPLICATION_XML_VALUE)
+	@PostMapping(value = "/convert", produces = MediaType.APPLICATION_XML_VALUE)
 	public String convert(@RequestParam("file") MultipartFile file) {
 		try {
 			Bpmn2secbpmnbc runner = new Bpmn2secbpmnbc();
@@ -59,21 +71,22 @@ public class Application {
 			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 			runner.saveModels(outputStream);
 			return outputStream.toString();
-	} catch (ATLCoreException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	} catch (ATLExecutionException e) {
-		e.printStackTrace();
-	}return null;
+		} catch (ATLCoreException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ATLExecutionException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
+
 	@PostMapping("/check")
 	public String check(@RequestParam("file") MultipartFile file) throws IOException {
 		InferBCProperties infer = new InferBCProperties();
 		java.nio.file.Path path = Paths.get("temp.secbpmn2bc");
 		file.transferTo(path);
-		
+
 		// Create a resource set to hold the resources.
 		//
 		ResourceSet resourceSet = new ResourceSetImpl();
@@ -88,28 +101,25 @@ public class Application {
 		//
 		resourceSet.getPackageRegistry().put(SecBPMN2BCPackage.eNS_URI, SecBPMN2BCPackage.eINSTANCE);
 
-		
 		Resource resource = resourceSet.getResource(URI.createFileURI(path.toAbsolutePath().toString()), true);
-		
+
 		List<ConsoleMessage> list = null;
 
 		list = infer.checkConstraint(resource);
-		
+
 		resource.unload();
 		resource.delete(null);
-		
+
 		return list.toString();
-		
-		
+
 	}
-	
-	
+
 	@PostMapping("/annotate")
-	public String annotate(@RequestParam("file") MultipartFile file) throws IOException {
+	public String annotate(@RequestParam("file") MultipartFile file, @RequestParam(value = "override", required = false) boolean override) throws IOException {
 		InferBCProperties infer = new InferBCProperties();
 		java.nio.file.Path path = Paths.get("temp.secbpmn2bc");
 		file.transferTo(path);
-		
+
 		// Create a resource set to hold the resources.
 		//
 		ResourceSet resourceSet = new ResourceSetImpl();
@@ -124,42 +134,147 @@ public class Application {
 		//
 		resourceSet.getPackageRegistry().put(SecBPMN2BCPackage.eNS_URI, SecBPMN2BCPackage.eINSTANCE);
 
-		
 		Resource resource = resourceSet.getResource(URI.createFileURI(path.toAbsolutePath().toString()), true);
-		
+
 		List<ConsoleMessage> list = null;
 
-		list = infer.annotate(resource, true);
-		
+		System.out.println(override);
+		list = infer.annotate(resource, override);
+
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		resource.save(outputStream, null);
 		resource.unload();
 		resource.delete(null);
-		
-		if(!infer.checkForErrors(list)){
+
+		if (!infer.checkForErrors(list)) {
 			return outputStream.toString();
 		} else {
 			return list.toString();
 		}
-		
+
 	}
-	
-	private void updateModel(Document input, Document result){
+
+	@PostMapping("/updateModel")
+	public String updateModel(@RequestParam("bpmnfile") MultipartFile bpmnfile,
+			@RequestParam("secbpmnfile") MultipartFile secbpmnfile)
+			throws ParserConfigurationException, IOException, SAXException, TransformerException {
 		XPathFactory xpathFactory = XPathFactory.newInstance();
-	    XPath xpath = xpathFactory.newXPath();
-	    
-	    try {
+		XPath xpath = xpathFactory.newXPath();
 
-	      XPathExpression expr = xpath.compile("//*");
-	      NodeList nodes = (NodeList) expr.evaluate(result, XPathConstants.NODESET);
-	      for (int i = 0; i < nodes.getLength(); i++) {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
 
-	        
-	       //Customize the code to fetch the value based on the node type and hierarchy 
-	        
-	      }
-	    } catch (XPathExpressionException e) {
-	      e.printStackTrace();
-	    }
+		java.nio.file.Path secbpmnpath = Paths.get("temp.secbpmn2bc");
+		secbpmnfile.transferTo(secbpmnpath);
+		Document secbpmndoc = db.parse(secbpmnpath.toFile());
+		
+		java.nio.file.Path bpmnpath = Paths.get("temp.bpmn");
+		bpmnfile.transferTo(bpmnpath);
+		Document bpmndoc = db.parse(bpmnpath.toFile());
+
+		try {
+			
+			/* Handle blockchainType property */
+			XPathExpression expr = xpath.compile("//*[@BlockchainType]");
+			NodeList srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("update invoked");
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[local-name()='collaboration']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:blockchainType", srcnodes.item(i).getAttributes().getNamedItem("BlockchainType").getNodeValue());
+				}
+			}
+			/* Handle onChainModel for definitions */
+			expr = xpath.compile("//*[@OnChainModel][local-name()='Definitions']");
+			srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[local-name()='collaboration']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:onChainModel", srcnodes.item(i).getAttributes().getNamedItem("OnChainModel").getNodeValue());
+				}
+			}
+			/* Handle onChainModel for processes */
+			expr = xpath.compile("//*[@OnChainModel][@type='secbpmn2bc:Process']");
+			srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[@processRef='" + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue() + "']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:onChainModel", srcnodes.item(i).getAttributes().getNamedItem("OnChainModel").getNodeValue());
+				}
+			}
+			/* Handle onChainModel for subprocesses */
+			expr = xpath.compile("//*[@OnChainModel][@type='secbpmn2bc:SubProcess']");
+			srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[@id='" + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue() + "']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:onChainModel", srcnodes.item(i).getAttributes().getNamedItem("OnChainModel").getNodeValue());
+				}
+			}
+			/* Handle onChainExecution */
+			expr = xpath.compile("//*[@OnChainExecution]");
+			srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[@id='" + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue() + "']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:onChainExecution", srcnodes.item(i).getAttributes().getNamedItem("OnChainExecution").getNodeValue());
+				}
+			}
+			/* Handle onChainData for data objects */
+			expr = xpath.compile("//*[@OnChainData][@type='secbpmn2bc:DataObject']");
+			srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[@id='" + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue() + "']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:onChainData", srcnodes.item(i).getAttributes().getNamedItem("OnChainData").getNodeValue());
+				}
+			}
+			/* Handle onChainData for messages */
+			expr = xpath.compile("//*[@OnChainData][@type='secbpmn2bc:MessageRef']");
+			srcnodes = (NodeList) expr.evaluate(secbpmndoc, XPathConstants.NODESET);
+			for (int i = 0; i < srcnodes.getLength(); i++) {
+				System.out.println("Found " + srcnodes.item(i).getAttributes().getNamedItem("uuid").getNodeValue());
+				System.out.println("Parent is " + srcnodes.item(i).getParentNode().getAttributes().getNamedItem("uuid").getNodeValue());
+				XPathExpression expr2 = xpath.compile("//*[@id='" + srcnodes.item(i).getParentNode().getAttributes().getNamedItem("uuid").getNodeValue() + "']");
+				NodeList tnodes = (NodeList) expr2.evaluate(bpmndoc, XPathConstants.NODESET);
+				for (int j = 0; j < tnodes.getLength(); j++) {
+					System.out.println("Found " + tnodes.item(j).getAttributes().getNamedItem("id").getNodeValue());
+					Element telem = (Element) tnodes.item(j);
+					telem.setAttribute("sec:onChainData", srcnodes.item(i).getAttributes().getNamedItem("OnChainData").getNodeValue());
+				}
+			}
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(bpmndoc), new StreamResult(writer));
+		
+		return writer.getBuffer().toString();
 	}
 }
